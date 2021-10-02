dbdc49786be873a47b7731bbf45286c9be450bf8
mrmda28 <mrmda28@gmail.com>
2021-10-03 02:13:28 +0300

    Initial commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Dmitriy Maslennikov on 03/10/2021.
+//  Copyright © 2021 Dmitriy Maslennikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
